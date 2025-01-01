e5e091acb37aff6c9b40dd43861390a66471ef4e
Byrone Kinsly <Kinslybyrone17@gmail.com>
2025-01-02 00:47:00 +0300

    updated the popular recipes for better UI and added the add to favorite iconButton

--- lib/components/recipe_card.dart
+++ lib/components/recipe_card.dart
@@ -1,9 +1,17 @@
+
 import 'package:flutter/material.dart';
 import 'package:recipe/constants/constants.dart';
 
-class RecipeCard extends StatelessWidget {
+class RecipeCard extends StatefulWidget {
   final String imagePath;
   const RecipeCard({super.key, required this.imagePath});
+
+  @override
+  State<RecipeCard> createState() => _RecipeCardState();
+}
+
+class _RecipeCardState extends State<RecipeCard> {
+  bool isFavorite = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,35 +22,49 @@ class RecipeCard extends StatelessWidget {
           Container(
             width: double.infinity,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
+              borderRadius:
+                  const BorderRadius.only(topRight: Radius.circular(20)),
               color: primaryColor,
+              image: DecorationImage(
+                image: AssetImage(
+                  widget.imagePath,
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+            foregroundDecoration: const BoxDecoration(
+              color: Color.fromRGBO(255, 102, 14, 0.2),
+              borderRadius: BorderRadius.only(topRight: Radius.circular(20)),
             ),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                const SizedBox(
-                  height: 15,
-                ),
-                Center(
-                  child: Image.asset(
-                    imagePath,
-                    height: 120,
-                    width: 120,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                const Padding(
-                    padding: EdgeInsets.only(left: 10),
-                    child: Text("Recipe Name",
-                        style: TextStyle(
-                          fontSize: 12,
-                        ))),
-                const SizedBox(
-                  height: 5,
-                ),
+                Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(color: Colors.black),
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Recipe Name",
+                          style: TextStyle(fontSize: 13, color: Colors.white),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isFavorite = isFavorite ? false : true;
+                            });
+                          },
+                          icon: Icon(
+                            isFavorite
+                                ? Icons.favorite
+                                : Icons.favorite_outline,
+                            color: isFavorite ? mainColor : Colors.white,
+                          ),
+                        )
+                      ],
+                    )),
               ],
             ),
           )
